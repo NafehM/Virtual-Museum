99a525842ab246e7afa52bc4bca4ff3c87533b78
Nafeh Mahmoud <72032482+NafehM@users.noreply.github.com>
2024-05-09 14:58:40 +0100

    Sound fixed

--- Assets/_Course Library/Scripts/Actions/SoundPlayer.cs
+++ Assets/_Course Library/Scripts/Actions/SoundPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SoundPlayer : MonoBehaviour
 {
     public AudioClip clip;
-    AudioSource audio;
+    private new AudioSource audio;
 
     // Start is called before the first frame update
     void Start()
